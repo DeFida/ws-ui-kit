9a8803e78f51cc16d86d79ecc5ace7b26234888f
DeFida <dasiktazik@gmail.com>
2023-08-20 08:38:36 +0600

    To laoep

--- src/components/WSInput/WSInput.tsx
+++ src/components/WSInput/WSInput.tsx
@@ -14,7 +14,7 @@ interface WSInputProps extends InputHTMLAttributes<HTMLInputElement> {
 const WSInput: FC<WSInputProps> = ({ children, className='', required=false, label, id, name, ...props }) => {
     let componentClassName = `${styles.WSInput} ${className}`;
     
-    const inputRef = useRef<HTMLInputElement | null>(null);
+    // const inputRef = useRef<HTMLInputElement | null>(null);
     const [errorMessage, setErrorMessage] = useState<string | null>('You are an error!')
 
     return (
@@ -24,7 +24,7 @@ const WSInput: FC<WSInputProps> = ({ children, className='', required=false, lab
                 {required && <p>*</p>}
             </div>
 
-            <input id={id} ref={inputRef} name={name} className={componentClassName} {...props} />
+            <input id={id} name={name} className={componentClassName} {...props} />
             {errorMessage && <WSParagraph className={colorsStyles.secondary}>{errorMessage}</WSParagraph>}
         </div>
         
