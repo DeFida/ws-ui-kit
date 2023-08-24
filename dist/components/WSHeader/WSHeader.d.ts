50118d8323fcde16e2f1c0129f8db5ad95011202
DeFida <dasiktazik@gmail.com>
2023-08-24 19:09:27 +0600

    header

--- dist/components/WSHeader/WSHeader.d.ts
+++ dist/components/WSHeader/WSHeader.d.ts
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react';
 interface WSHeaderProps {
     children?: ReactNode;
     className?: string;
+    logo?: any;
 }
 declare const WSHeader: FC<WSHeaderProps>;
 export default WSHeader;
