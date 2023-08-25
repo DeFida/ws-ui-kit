9784afb14e4c97290a115143256773f970550388
DeFida <dasiktazik@gmail.com>
2023-08-25 12:09:44 +0600

    header

--- src/components/WSHeader/WSHeader.tsx
+++ src/components/WSHeader/WSHeader.tsx
@@ -6,19 +6,18 @@ import WSImage from '../WSImage/WSImage';
 interface WSHeaderProps {
     children?: ReactNode;
     className?: string;
-    logo?: any;
-    onLogoClick?: () => void;
+    logo?: ReactNode;
     buttons?: ReactNode;
 }
 
 
-const WSHeader: FC<WSHeaderProps> = ({children, className, onLogoClick, buttons, logo='https://www.weskill.kz/_next/static/media/weskill_logo.5af2785e.svg' }) => {
+const WSHeader: FC<WSHeaderProps> = ({children, className, buttons, logo }) => {
 
     const componentClassName = `${styles.WSHeader} ${className}`;
 
     return (
         <div className={`${componentClassName}`}>
-            <WSImage height={0} width={0} alt='logo' src={logo} onClick={onLogoClick} className={styles.WSHeader__logo} />
+            {logo}
             <div className={`${styles.WSHeader__buttons}`}>
                 {buttons}
             </div>
