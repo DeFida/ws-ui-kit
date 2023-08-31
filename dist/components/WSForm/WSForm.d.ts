7a6f64f3384d5e934515c2e10953a782e46a342a
DeFida <dasiktazik@gmail.com>
2023-08-31 18:14:02 +0600

    Form error was added

--- dist/components/WSForm/WSForm.d.ts
+++ dist/components/WSForm/WSForm.d.ts
@@ -2,7 +2,6 @@ import React, { ReactNode, FormHTMLAttributes, FormEvent } from 'react';
 interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
     children: ReactNode;
     width?: string;
-    error: string | null;
     onSubmit: (event: FormEvent<HTMLFormElement>) => void;
 }
 declare const WSForm: React.FC<FormProps>;
