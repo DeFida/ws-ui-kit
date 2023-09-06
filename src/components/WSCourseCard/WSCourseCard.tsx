7a011abdecb425dcec7d432e7c04cb7c8f26d793
DeFida <dasiktazik@gmail.com>
2023-09-06 14:13:46 +0600

    Course card was edited

--- src/components/WSCourseCard/WSCourseCard.tsx
+++ src/components/WSCourseCard/WSCourseCard.tsx
@@ -7,26 +7,30 @@ import WSParagraph from '../WSParagraph/WSParagraph';
 import WSImage from '../WSImage/WSImage';
 import fontsStyles from '../../styles/fonts.module.scss';
 
-interface CourseProp {
+type CourseType = {
     id: string;
     name: string;
     linkName: string;
     previewImage: string;
     previewImageOnHover: string;
     previewDescription: string;
+}
+
+interface CourseProp {
+    course: CourseType;
     className?: string;
 }
 
-const WSCourseCard: FC<CourseProp> = ({id, name, linkName, previewDescription, className, previewImage, previewImageOnHover, ...props}) => {
+const WSCourseCard: FC<CourseProp> = ({course, className, ...props}) => {
     const componentClassnames = `${styles.WSCourseCard} ${className}`
 
     return (
-        <a href={`/courses/${linkName}`} className={componentClassnames}>
-            <WSHeading type='h2' className={`${fontsStyles.regular} ${styles.cardTitle}`}>{name}</WSHeading>
-            <WSImage src={previewImage} width={0} height={0} className={`${styles.cardImage}`} alt='hoverImage' />
-            <WSImage src={previewImageOnHover} width={0} height={0} className={`${styles.onHoverCardImage}`} alt='hoverImage' />
+        <a href={`/courses/${course.linkName}`} className={componentClassnames}>
+            <WSHeading type='h2' className={`${fontsStyles.regular} ${styles.cardTitle}`}>{course.name}</WSHeading>
+            <WSImage src={course.previewImage} width={0} height={0} className={`${styles.cardImage}`} alt='hoverImage' />
+            <WSImage src={course.previewImageOnHover} width={0} height={0} className={`${styles.onHoverCardImage}`} alt='hoverImage' />
             
-            <WSParagraph className={`${styles.description}`}>{previewDescription}</WSParagraph>
+            <WSParagraph className={`${styles.description}`}>{course.previewDescription}</WSParagraph>
         </a>
     );
 };
--- src/stories/WSCourseCard.stories.ts
+++ src/stories/WSCourseCard.stories.ts
@@ -17,11 +17,13 @@ type Story = StoryObj<typeof meta>;
 
 export const CourseCard: Story = {
   args: {
-    id: '123',
-    name: "Python",
-    previewDescription: "python * telebot",
-    previewImage: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-    previewImageOnHover: "https://image.shutterstock.com/image-vector/dotted-spiral-vortex-royaltyfree-images-600w-2227567913.jpg",
-    linkName: "python"
+    course: {
+        id: '123',
+        name: "Python",
+        previewDescription: "python * telebot",
+        previewImage: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+        previewImageOnHover: "https://image.shutterstock.com/image-vector/dotted-spiral-vortex-royaltyfree-images-600w-2227567913.jpg",
+        linkName: "python"
+    }
   },
 };
