4e98de0dfe20d5324735875131e154f52377e58a
DeFida <dasiktazik@gmail.com>
2023-09-22 14:28:33 +0600

    mobile

--- src/components/WSForm/WSForm.tsx
+++ src/components/WSForm/WSForm.tsx
@@ -3,13 +3,12 @@ import styles from './WSForm.module.scss';
 
 interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
   children: ReactNode;
-  width?: string;
   onSubmit: (event: FormEvent<HTMLFormElement>) => void;
 }
 
-const WSForm: React.FC<FormProps> = ({ children, onSubmit, width = '400px', ...props }) => {
+const WSForm: React.FC<FormProps> = ({ children, onSubmit, ...props }) => {
     return (
-        <form className={`${styles.WSForm}`} onSubmit={onSubmit} style={{width}} {...props}>
+        <form className={`${styles.WSForm}`} onSubmit={onSubmit} {...props}>
             {children}
             
         </form>
