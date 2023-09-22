342b6cafe31c107ffda4d60d59191c46adf23182
DeFida <dasiktazik@gmail.com>
2023-09-22 14:37:17 +0600

    mobile

--- dist/components/WSForm/WSForm.d.ts
+++ dist/components/WSForm/WSForm.d.ts
@@ -2,6 +2,7 @@ import React, { ReactNode, FormHTMLAttributes, FormEvent } from 'react';
 interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
     children: ReactNode;
     onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+    className?: string;
 }
 declare const WSForm: React.FC<FormProps>;
 export default WSForm;
